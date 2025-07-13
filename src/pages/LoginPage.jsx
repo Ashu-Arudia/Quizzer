7c3d5c593d310d85e26afd1bf9585128340c07f8
Aditya Ashok Singh <adityaashoksingh7@gmail.com>
2025-07-13 11:22:22 +0530

    second commit

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,8 +3,13 @@ import './LoginPage.css';
 import LoginForm from '../components/LoginForm';
 export default function LoginPage() {
   return (
+    <>
+    <div style={{border: '2px solid black', borderRadius: '20px'}}>
     <div style={{ padding: 30 }}>
+    <h1 style={{textAlign: 'center',paddingBottom:'10px'}}>Quizz App</h1>
       <LoginForm />
     </div>
+    </div>
+    </>
   );
 }
